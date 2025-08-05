53054ad1129fd5a8306474446026e026af98b72d
yominmirin <longdoumanwa@gmail.com>
2025-08-05 17:41:55 +0900

    タスクモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_04_090048) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_05_082204) do
+  create_table "tasks", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.date "deadline"
+    t.integer "status_id"
+    t.integer "category_id"
+    t.integer "priority_id"
+    t.boolean "needs_editing", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_04_090048) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
