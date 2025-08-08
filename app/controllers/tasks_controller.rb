eaabe7c510c61d90244d7b2db161e70821bf39d4
yominmirin <longdoumanwa@gmail.com>
2025-08-08 15:49:54 +0900

    タスク削除機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,23 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    respond_to do |format|
+      if request.referer&.include?(task_path(@task))
+        # 詳細ページから削除された場合 → 一覧ページへリダイレクト
+        format.turbo_stream { redirect_to tasks_path, notice: "タスクを削除しました" }
+        format.html { redirect_to tasks_path, notice: "タスクを削除しました" }
+      else
+        # 一覧ページから削除された場合 → 部分更新
+        format.turbo_stream
+        format.html { redirect_to tasks_path, notice: "タスクを削除しました" }
+      end
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :category_id, :deadline, :priority_id, :details, :needs_editing).merge(user_id: current_user.id)
