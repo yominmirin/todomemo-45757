9815d3aaa6ae6c1eeb7affc1de2f543d13020eef
yominmirin <longdoumanwa@gmail.com>
2025-08-12 16:47:28 +0900

    rubocopの実行

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,68 +20,70 @@ class TasksController < ApplicationController
   def show
   end
 
-def edit
-  # Turbo Frameからのリクエストの場合
-  if turbo_frame_request?
-    # edit.html.erbをレンダリングする
-    render :edit, layout: false
-  else
-    # それ以外（通常のページ遷移など）の場合は、editビューをレンダリングする
-    render :edit
+  def edit
+    # Turbo Frameからのリクエストの場合
+    if turbo_frame_request?
+      # edit.html.erbをレンダリングする
+      render :edit, layout: false
+    else
+      # それ以外（通常のページ遷移など）の場合は、editビューをレンダリングする
+      render :edit
+    end
   end
-end
 
-def update
-  if @task.update(task_params)
-    if %w[cancel true false].include?(params[:task][:needs_editing])
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.replace("task-show-frame", partial: "tasks/show_frame", locals: { task: @task }),
-            turbo_stream.append("body", "<script>updateLayout();</script>")
-          ]
+  def update
+    if @task.update(task_params)
+      if %w[cancel true false].include?(params[:task][:needs_editing])
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: [
+              turbo_stream.replace('task-show-frame', partial: 'tasks/show_frame', locals: { task: @task }),
+              turbo_stream.append('body', '<script>updateLayout();</script>')
+            ]
+          end
+          format.html { redirect_to @task, notice: 'タスクが正常に更新されました。' }
         end
-        format.html { redirect_to @task, notice: "タスクが正常に更新されました。" }
+      else
+        # もしneeds_editingが含まれない場合の処理（例：普通に更新完了してshowへリダイレクト）
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: [
+              turbo_stream.replace('task-show-frame', partial: 'tasks/show_frame', locals: { task: @task }),
+              turbo_stream.append('body', '<script>updateLayout();</script>')
+            ]
+          end
+          format.html { redirect_to @task, notice: 'タスクが正常に更新されました。' }
+        end
       end
     else
-      # もしneeds_editingが含まれない場合の処理（例：普通に更新完了してshowへリダイレクト）
       respond_to do |format|
         format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.replace("task-show-frame", partial: "tasks/show_frame", locals: { task: @task }),
-            turbo_stream.append("body", "<script>updateLayout();</script>")
-          ]
+          render turbo_stream: turbo_stream.replace('edit_task', partial: 'tasks/edit_form', locals: { task: @task }),
+                 status: :unprocessable_entity
         end
-        format.html { redirect_to @task, notice: "タスクが正常に更新されました。" }
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
-  else
+  end
+
+  def destroy
+    @task.destroy
+
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("edit_task", partial: "tasks/edit_form", locals: { task: @task }), status: :unprocessable_entity
+      if request.referer&.include?(tasks_path) # 一覧ページからの削除
+        format.turbo_stream
+        format.html { redirect_to tasks_path, notice: '削除しました' }
+      else # 詳細ページなどからの削除
+        format.html { redirect_to tasks_path, notice: '削除しました' }
       end
-      format.html { render :edit, status: :unprocessable_entity }
     end
   end
-end
-
-def destroy
-  @task.destroy
-
-  respond_to do |format|
-    if request.referer&.include?(tasks_path) # 一覧ページからの削除
-      format.turbo_stream
-      format.html { redirect_to tasks_path, notice: "削除しました" }
-    else # 詳細ページなどからの削除
-      format.html { redirect_to tasks_path, notice: "削除しました" }
-    end
-  end
-end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :category_id, :deadline, :priority_id, :details, :status_id, :needs_editing).merge(user_id: current_user.id)
+    params.require(:task).permit(:title, :category_id, :deadline, :priority_id, :details, :status_id,
+                                 :needs_editing).merge(user_id: current_user.id)
   end
 
   def set_task
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :task do
-    title         { "taskname" }
-    details       { "taskinfo" }
+    title         { 'taskname' }
+    details       { 'taskinfo' }
     deadline      { Date.today + 7.days }
-    status_id     {1}
-    category_id   {1}
-    priority_id   {1}
+    status_id     { 1 }
+    category_id   { 1 }
+    priority_id   { 1 }
     needs_editing { false }
   end
 end
