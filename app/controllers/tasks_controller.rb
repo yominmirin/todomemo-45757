738fcb5a197748d11b845791120588efa60a83ec
yominmirin <longdoumanwa@gmail.com>
2025-08-12 18:21:10 +0900

    ログインしているユーザーのタスクのみを表示するように変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.where(user_id: current_user.id).order(created_at: :desc)
   end
 
   def new
